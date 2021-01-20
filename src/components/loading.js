73c8896b8381a8982d79b2e5ef576f91d3f90dd1
nicotorresel <nicotorresel@gmail.com>
2021-01-20 14:26:43 -0300

    consuming api

---
+++ src/components/loading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './loading.css';
+
+const Loading = () => {
+  return(
+    <React.Fragment>
+      <div className="upp">
+        <div className="col-md-12">
+          <h3>Loading...</h3>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Loading;
--- src/components/search-result.js
+++ src/components/search-result.js
@@ -1,23 +1,35 @@
 import React from 'react';
+import ArtistCard from './/artist-card';
+import Loading from './loading';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class SearchResult extends React.Component{
 
   state = {
-    data: ""
+    loading: false,
+    data: {
+      similarartists: {
+        artist:[]
+      }
+    }
   };
 
   componentDidMount() {
     this.fetchData(
-      'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=korn&api_key=65bd337131a93bd0f0550b29526413ef&format=json'
+      'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=madona&api_key=65bd337131a93bd0f0550b29526413ef&format=json'
     );
   }
 
   fetchData = async url =>{
+    this.setState({
+      loading: true
+    });
+
     const response = await fetch(url);
     const data = await response.json();
     console.log(data, 'lo que trae la api');
     this.setState({
+      loading: false,
       data: data
     });
   };
@@ -25,11 +37,18 @@ class SearchResult extends React.Component{
   render() {
     return (
       <React.Fragment>
+        {this.state.loading && <Loading />}
         <div className="container">
           <div className="row">
-            {/* {this.state.artist.map((artista, i)=>{
-              return <ArtistCard img={artista.image} titulo={artista.name} alt={artista.name} key={i}/>
-            })} */}
+            {this.state.data.similarartists.artist.map((artista, i)=>{
+              return (
+                <ArtistCard 
+                  img={artista.image[2]["#text"]} 
+                  titulo={artista.name} 
+                  alt={artista.name} key={i}
+                />
+              )
+            })}
             <h1>{this.props.busqueda}</h1> 
           </div>
         </div>
